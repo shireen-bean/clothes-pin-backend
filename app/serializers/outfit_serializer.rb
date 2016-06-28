f8e1a1501f1a96f9814dbe5e23843080ad80c067
shkherad <shkherad@bu.edu>
2016-06-28 09:41:16 -0400

    tinker
    tinker

--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -1,3 +1,3 @@
 class OutfitSerializer < ActiveModel::Serializer
-  attributes :id, :wear_on, :description, :shirt_id, :bottom_id, :accessory_id, :shoe_id, :profile_id
+  attributes :id, :wear_on, :description, :shirt_id, :bottom_id, :accessory_id, :shoe_id, :profile_id, :date
 end
